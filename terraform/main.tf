45b3fa7e269cd913486c15cebc84caaa069c2ac2
Jamil Khan <jamilkhan.nu@gmail.com>
2026-08-21 19:06:35 +0100

    added debug step

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ terraform {
     }
   }
 
-  # Remote state — remote bucket- this is a comments
+  # Remote state — remote bucket- this is a comments as
   backend "s3" {
     bucket         = "aws-terraform-githubaction-project-state-bucket"
     key            = "oidc-demo/terraform.tfstate"
